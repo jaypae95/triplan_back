10940d6f67d4e21d671de1bf829f08347922f897
jaypae95 <jaypae95@hbsmith.io>
2019-11-20 18:52:27 +0900

    example of data receiving & sending

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+  next();
 });
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  console.log(req.body)
+  res.send(req.body)
+});
+
 
 module.exports = router;
