c7f7b4323c893bbc612c6e9b269a33ac07f30f3f
lem0n <kangjieun9843@ajou.ac.kr>
2019-12-05 11:23:56 +0900

    overlapId

--- routes/users.js
+++ routes/users.js
@@ -42,12 +42,18 @@ router.post('/login', function (req, res, next) {
 router.post('/signup', function (req, res, next) {
   db.connection.query("INSERT INTO triplan.UserInfo (user_id,user_name,user_gender,user_phone,user_email,user_password) VALUES (?,?,?,?,?,?)",
     [req.body.user_id, req.body.user_name, req.body.user_gender, req.body.user_phone, req.body.user_email, bcrypt.hashSync(req.body.user_password, 8)], function (err, r, fields) {
-      if (r) {
-        res.json({success: true});
-      } else {
-        res.json({success: false})
-      }
+      
     });
 });
 
+router.post('/overlap', function (req, res, next) {
+  db.connection.query("SELECT * FROM triplan.UserInfo WHERE user_id=?", req.body.user_id, function (err, r) {
+    console.log(r)
+    if (r.length>0){
+      res.json({is_exist: true})
+    }else {
+      res.json({is_exist:false})
+    }
+  });
+});
 module.exports = router;
