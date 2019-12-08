231ffcba731354d1659ce76fe801c362378610e6
jaypae95 <jaypae95@hbsmith.io>
2019-12-08 14:17:55 +0900

    hotfix remove console.log

--- routes/users.js
+++ routes/users.js
@@ -18,13 +18,11 @@ function getUser(req, res) {
   if (req.headers && req.headers.authorization) {
     let authorization = req.headers.authorization
     let decoded = ''
-    console.log(authorization)
     try {
       decoded = jwt.verify(authorization, process.env.JWT_SECRET);
     } catch (e) {
       return {detail: 'unauthorized'}
     }
-    console.log(decoded)
     return {detail: 'success', user: decoded.data}
     // Fetch the user by id
   }
